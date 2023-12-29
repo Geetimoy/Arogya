8fb173caf3892c7bfde5cbcb812fae5290789459
GeetimoyOfficePC145 <geetimoy.sahu@gmail.com>
2023-12-29 18:35:45 +0530

    Add new files

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -7,8 +7,44 @@ function ContactUs(){
   return(
     <>
       <AppTop></AppTop>
-      <div className="app-body">
+      <div className="app-body contact-us">
         <h5 className="title">Conatct Us</h5>
+        <p className="mb-0"><strong>Read Me</strong></p>
+        <p>To report any problem, please fill this form below. Our Support Team or Admin Team will look into the issue(s) as soon as possile.</p>
+        <p className="mb-0"><strong>Contact Person: </strong></p>
+        <ul className="p-0">
+          <li>Contact Number: 9577793751</li>
+          <li>Email: arindam.deka7@gmail.com</li>
+        </ul>
+        <h5 className="title">Create New Case / Issue / Problem</h5>
+        <form className="contactus-form">
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="normal-box mt-2"><span>Patient ID:</span>UP0033445576</div>
+            </div>
+          </div>
+          <div className="form-group">
+            <label for="name">Problem Type: </label>
+            <select id="" name="" className="form-control">
+              <option value="">Select</option>
+              <option value="1">Can't book new appoinment</option>
+              <option value="2">Can't cancel an appoinment</option>
+              <option value="3">Can't download prescription</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <label for="name">Subject: </label>
+            <input type="text" className="form-control" name="" id=""></input>
+          </div>
+          <div className="form-group">
+            <label for="name">Describe / Explain Problem: </label>
+            <textarea name="" id="" rows="3"  className="form-control"></textarea>
+          </div>
+          <div className='btns-group d-flex justify-content-center'>
+            <button type="button" id="" name="" className="btn btn-primary primary-bg-color border-0 mx-2">Submit</button>
+            <button type="button" class="btn btn-primary primary-bg-color border-0 mx-2">Cancel</button>
+          </div>
+        </form>
       </div>
       <Appfooter></Appfooter>
     </>
--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -1,13 +1,30 @@
 import Appfooter from "./AppFooter";
 import AppTop from "./AppTop";
 
+import './Feedback.css'
+
 function Feedback(){
   return(
     <>
       <AppTop></AppTop>
-        <div className="app-body">
+        <div className="app-body feedback">
           <h5 className="title">Feedback</h5>
-          <p>Aliquam gravida placerat nibh ac ullamcorper. Morbi viverra pretium lorem, eget vestibulum massa. Morbi eleifend tempor ante, nec dictum urna tristique id. Sed euismod mauris non magna tempor hendrerit. Vivamus elit nisl, blandit maximus neque interdum, tempus pretium orci. Proin id est sapien.</p>
+          <p className="mb-0"><strong>Read Me</strong></p>
+          <p className="mb-0 small">Your feedback is important to us. It helps us improve RGVN TeleHealth. </p>
+          <p className="mb-0 small">How easy or difficult was it for you to use RGVN TeleHealth?</p>
+          <p className="mb-0 small">How would you rate your experience with RGVN TeleHealth Service?</p>
+          <form className="feedback-form">
+            <div className="row">
+              <div className="col-lg-12">
+                <div className="normal-box mt-2"><span>Patient ID:</span>UP0033445576</div>
+              </div>
+              <div className="col-lg-12">
+                <div className="normal-box mt-2"><span>How easy was the app to use:</span>
+                    <div><button type="button" class="btn btn-outline-info waves-effect waves-light">Very Easy</button></div>
+                </div>
+              </div>
+            </div>
+          </form>
         </div>
       <Appfooter></Appfooter>
     </>
