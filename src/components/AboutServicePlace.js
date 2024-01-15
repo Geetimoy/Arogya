d54f8139751bc91097d1da1cec915f829faadfbb
GeetimoyOfficePC145 <geetimoy.sahu@gmail.com>
2024-01-15 21:25:30 +0530

    About ServicePlace Dynamic

--- src/components/AboutServicePlace.js
+++ src/components/AboutServicePlace.js
@@ -5,21 +5,59 @@ import Appfooter from "./AppFooter";
 
 import "./AboutServicePlace.css"
 
+//import SystemContext from '../context/system/SystemContext';
+
+import { API_URL } from "./util/Constants";
+
 function AboutServicePlace(){
+  //const systemContext = useContext(SystemContext);
 
-  const [post, setPost] = useState([]);
+  const [data, setData] = useState({page_content:'', page_title:''});
+  
+  let jsonData = {};
 
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts?_limit=1")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setPost(data);
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
-  }, []);
+      jsonData["page_key"] = localStorage.getItem('page_key');
+      jsonData["system_id"] = "telehealth.serviceplace.org.in";
+
+      useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const response = await fetch(`${API_URL}/staticPage`, {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify(jsonData),
+            });
+            
+            const responseData = await response.json();
+            // console.log(responseData.data.results[0]);
+            setData(responseData.data.results[0]);
+            // console.log('Hi');
+            console.log(data);
+          } catch (error) {
+            console.error('Error fetching data:', error);
+          } finally {
+            // setLoading(false);
+          }
+        };
+    
+        fetchData();
+      }, []);
+
+  // const [post, setPost] = useState([]);
+
+  // useEffect(() => {
+  //   fetch("https://jsonplaceholder.typicode.com/posts?_limit=1")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       console.log(data);
+  //       setPost(data);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err.message);
+  //     });
+  // }, []);
 
   return(
     
@@ -27,8 +65,9 @@ function AboutServicePlace(){
       <AppTop></AppTop>
     
       <div className="app-body">
-        <h5 className='title'>About Service Place</h5>
-        <p>Integer sagittis enim at nulla imperdiet porttitor. Cras ultrices, urna quis tincidunt porta, mauris nisl pretium nibh, vitae lacinia quam dui et augue. </p>
+        <h5 className='title'>{data.page_title}</h5>
+        {/* <h5>{systemContext.systemDetails.thp_domain_name}</h5> */}
+        <p>{data.page_content} </p>
 
         {/* {post.map((post) => {
           return (
