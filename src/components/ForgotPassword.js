9377eea2f72e5f2c650c739b042b43679b8fdbd3
Manish Prasad <mprasad75@gmail.com>
2024-01-18 22:55:36 +0530

    Forgot Password

--- src/components/ForgotPassword.js
+++ src/components/ForgotPassword.js
@@ -9,11 +9,94 @@ import { faLongArrowAltLeft } from '@fortawesome/free-solid-svg-icons';
 
 import { Link } from "react-router-dom";
 import SystemContext from "../context/system/SystemContext";
-import { useContext } from 'react';
+import AlertContext from "../context/alert/AlertContext";
+import { API_URL, ENCYPTION_KEY, DEVICE_TYPE, DEVICE_TOKEN } from "./util/Constants";
+import { useContext, useState } from "react";
+import CryptoJS from "crypto-js";
 
 function ForgotPassword(){
 
   const systemContext = useContext(SystemContext);
+  const alertContext = useContext(AlertContext);
+
+  const [formData, setFormData] = useState({
+    userId: {required: true, value:"", errorClass:"", errorMessage:""},
+  });
+
+  const resetForm = () => {
+    Object.keys(formData).forEach((element) => {
+      formData[element].value = "";
+      formData[element].errorMessage = "";
+      formData[element].errorClass = "";
+    })
+    setFormData({...formData, ...formData});
+  }
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    if(value.trim() !== ""){
+      setFormData({...formData, [name]: {...formData[name], value:value, errorClass:"", errorMessage:""}});
+    }
+    else{
+      setFormData({...formData, [name]: {...formData[name], value:value, errorClass:"form-error", errorMessage:"This field is required!"}});
+    }
+  }
+
+  const validateForm = () => {
+    const fieldName = Object.keys(formData);
+    let errorCounter = 0;
+    fieldName.forEach((element) => {
+      if(formData[element].required && formData[element].value.trim() === ""){
+        formData[element].errorMessage = "This field is required!";
+        formData[element].errorClass = "form-error";
+        errorCounter++;
+      }
+      else{
+        formData[element].errorMessage = "";
+        formData[element].errorClass = "";
+      }
+    })
+    setFormData({...formData, ...formData});
+    return errorCounter;
+  }
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    let errorCounter = validateForm();
+    if(errorCounter > 0){
+      return false;
+    }
+    else{
+
+      let jsonData = {};
+
+      jsonData['user_login_id'] = formData["userId"].value;
+      jsonData['system_id']     = systemContext.systemDetails.system_id;
+      jsonData['device_type']   = DEVICE_TYPE;
+      jsonData['device_token']  = DEVICE_TOKEN;
+      jsonData['user_lat']      = localStorage.getItem('latitude');
+      jsonData['user_long']     = localStorage.getItem('longitude');
+
+      const response = await fetch(`${API_URL}/forgotPassword`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(jsonData),
+      });
+      let result = await response.json();
+      
+      if (result.success) {
+        resetForm();
+        alertContext.setAlertMessage({show:true, type: "success", message: result.msg});
+      } 
+      else {
+        alertContext.setAlertMessage({show:true, type: "error", message: result.msg});
+      }
+
+    }
+  }
+
 
   return(
     <div className='container'>
@@ -26,12 +109,15 @@ function ForgotPassword(){
           <img src={systemContext.systemDetails.thp_app_logo_url} className="mb-3" alt={systemContext.systemDetails.thp_system_name} />
           <h5 className='title'>Reset Password</h5>
           <p>Enter your User ID and an OTP will be sent to your registered Email ID & Mobile Number </p>
-          <form>
-            <div className='form-group'>
+          <form onSubmit={handleFormSubmit}> 
+            <div className={`form-group ${formData["userId"].errorClass}`}>
               <label htmlFor='user_id'>User ID</label>
-              <input type="text" name="" id="user_id" className='form-control' placeholder="Enter user id" />
+              <input type="text" name="userId" id="userId" className='form-control' placeholder="Enter user id" onChange={handleChange} value={formData["userId"].value}/>
+              <small className="error-mesg">{formData["userId"].errorMessage}</small>
             </div>
-            <div className='btn primary-bg-color mb-3 mt-3 w-100'><Link to ="/login" className='m-auto text-light text-decoration-none d-block'>Submit</Link></div>
+            <div className='btn primary-bg-color mb-3 mt-3 w-100'>
+              <button type="submit" className='btn primary-bg-color text-light w-100'>Submit</button>
+            </div>
             <p className='mb-4'>I remember my password! Back me to <Link to="/login" className="primary-color">login</Link> page </p>
             <p className="text-center link-red mb-3">
               Don't have an account yet? 
