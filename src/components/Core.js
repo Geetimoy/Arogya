18506284d055c6da6f9fa151c9582062117b0dc0
GeetimoyOfficePC145 <geetimoy.sahu@gmail.com>
2024-01-26 19:42:35 +0530

    chnages

--- src/components/Core.js
+++ src/components/Core.js
@@ -33,10 +33,10 @@ import { API_URL, DEVICE_TYPE, DEVICE_TOKEN } from './util/Constants';
 export default function Core() {
 
   //const domainName = window.location.hostname;
-  const domainName = 'ukhra.serviceplace.org.in';
+  //const domainName = 'ukhra.serviceplace.org.in';
   // const domainName = 'b2h.serviceplace.org.in';
   // const domainName = 'rgvn.serviceplace.org.in';
-  //const domainName = 'telehealth.serviceplace.org.in';
+  const domainName = 'telehealth.serviceplace.org.in';
   const [systemId, setSystemId] = useState(null);
 
   const loginContext  = useContext(LoginContext);
