09b9bb7bd68fdf6b1837234487620c973a83928e
Manish Prasad <mprasad75@gmail.com>
2024-02-24 22:58:44 +0530

    Certificate Upload

--- src/components/UploadCertificates.js
+++ src/components/UploadCertificates.js
@@ -3,11 +3,15 @@ import { useState } from 'react';
 import Appfooter from "./AppFooter";
 import InnerAppTop from "./InnerAppTop";
 
+import { MAX_CERTICATE_UPLOAD } from './util/Constants';
+
 import './UploadCertificates.css'
 
 function UploadCertificates(){
 
- 
+  const uploadCertificateChange = (elem) => {
+    console.log(elem);
+  };
   
   return(
     <>
@@ -16,31 +20,13 @@ function UploadCertificates(){
       <div className='d-flex justify-content-between align-items-center'><h5>Upload Certificates </h5><span>Max 5 files, 500 KB each</span></div>
       <div className="upload-certificate-list">
         <div className="rounded jumbotron p-3 mt-3 mb-3">
-          <div className="form-group brdr-btm parent">
-            <input type="file" name="" id="" className="" />
-            <label>Upload Certificate 1</label>
+
+          {[...Array(MAX_CERTICATE_UPLOAD)].map((e, i) => <div key={i+1} className="form-group brdr-btm parent">
+            <input type="file" name={`certificate_${i+1}`} id={`certificate_${i+1}`} onChange={() => uploadCertificateChange('certificate_'+(i+1))}/>
+            <label>Upload Certificate {i+1}</label>
             <span className="close float-end">&#10006;</span>
-          </div>
-          <div className="form-group brdr-btm parent">
-            <input type="file" name="" id="" className="" />
-            <label>Upload Certificate 2</label>
-            <span className="close float-end">&#10006;</span>
-          </div>
-          <div className="form-group brdr-btm parent">
-            <input type="file" name="" id="" className="" />
-            <label>Upload Certificate 3</label>
-            <span className="close float-end">&#10006;</span>
-          </div>
-          <div className="form-group brdr-btm parent">
-            <input type="file" name="" id="" className="" />
-            <label>Upload Certificate 4</label>
-            <span className="close float-end">&#10006;</span>
-          </div>
-          <div className="form-group brdr-btm parent">
-            <input type="file" name="" id="" className="" />
-            <label>Upload Certificate 5</label>
-            <span className="close float-end">&#10006;</span>
-          </div>
+          </div>)}
+
           {/* <div className="form-group">
           <button type="button" id="" name="" class="btn btn-primary primary-bg-color border-0 mx-2">Update Photo<input type="file" name="cover" accept="img/*" style={{ display: "none" }} /></button>
           </div> */}
