3fc1f891e13778bdb6a72e5ae6efc46ce9d8d861
Manish Prasad <mprasad75@gmail.com>
2024-03-23 21:28:31 +0530

    Notifcations No record found

--- src/components/Notifications.js
+++ src/components/Notifications.js
@@ -107,7 +107,6 @@ function Notifications(){
             </div>}
         </div>
         <Modal show={showModal} onHide={modalClose}>  
-          
           <Modal.Body>  
             <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>  
           </Modal.Body>  
