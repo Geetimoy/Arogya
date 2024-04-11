d2a709a9d5c4c204876a1e5d3e5a167594714c94
Manish Prasad <mprasad75@gmail.com>
2024-04-11 22:58:16 +0530

    File Upload Done

--- src/components/UploadCertificates.js
+++ src/components/UploadCertificates.js
@@ -24,16 +24,16 @@ function UploadCertificates(){
   const [isMActive, setIsMActive] = useState(false);
 
   const [fileUpload, setFileUpload] = useState({
-    certificate_1 : {'upload': false, 'fileName': ''},
-    certificate_2 : {'upload': false, 'fileName': ''},
-    certificate_3 : {'upload': false, 'fileName': ''},
-    certificate_4 : {'upload': false, 'fileName': ''},
-    certificate_5 : {'upload': false, 'fileName': ''}
+    certificate_1 : {'upload': true, 'fileName': ''},
+    certificate_2 : {'upload': true, 'fileName': ''},
+    certificate_3 : {'upload': true, 'fileName': ''},
+    certificate_4 : {'upload': true, 'fileName': ''},
+    certificate_5 : {'upload': true, 'fileName': ''}
   });
 
-  useEffect(() => {
-    setFileUpload({...fileUpload, 'certificate_1': {...fileUpload['certificate_1'], upload:true, fileName:''}});
-  }, []);
+  // useEffect(() => {
+  //   setFileUpload({...fileUpload, 'certificate_1': {...fileUpload['certificate_1'], upload:true, fileName:''}});
+  // }, []);
 
   const handle2Click = () => {
     setIsMActive(!isMActive); // Toggle the state
@@ -76,42 +76,45 @@ function UploadCertificates(){
       
       if(result1.success){
         var uploadedCertificate = result1.data;
+        var uploadedSequence    = [];
         if(uploadedCertificate.length > 0){
           uploadedCertificate.forEach((file, index)=>{
             var fileName = file.replace(/\.[^/.]+$/, "");
             var sequence = fileName.replace(decryptedLoginDetails.account_key+'_', '');
-            fileUpload[sequence].upload   = true;
-            fileUpload[sequence].fileName = file;
-            setFileUpload({...fileUpload, ...fileUpload});
-            var nextFileIndex = index+1; 
-            if(nextFileIndex == uploadedCertificate.length)
-            {
-              nextFileIndex++;
-              if(nextFileIndex <= 5)
-              {
-                fileUpload['certificate_'+nextFileIndex].upload   = true;
-                fileUpload['certificate_'+nextFileIndex].fileName = '';
-                setFileUpload({...fileUpload, ...fileUpload});
-              }
-            }
+            uploadedSequence.push(sequence);
           })
+
+          for(var x=1; x<=5; x++)
+          {
+            var fileSequence = 'certificate_'+x;
+            if(uploadedSequence.includes(fileSequence))
+            {
+              var currentFileName = '';
+              uploadedCertificate.forEach((item)=>{
+                if(item.indexOf(fileSequence) !== -1){
+                  currentFileName = item;
+                }
+              })
+
+              fileUpload['certificate_'+x].upload   = true;
+              fileUpload['certificate_'+x].fileName = currentFileName;
+              setFileUpload({...fileUpload, ...fileUpload});
+            }
+            else
+            {
+              fileUpload['certificate_'+x].upload   = true;
+              fileUpload['certificate_'+x].fileName = '';
+              setFileUpload({...fileUpload, ...fileUpload});
+            }
+          }
         }
         else
         {
           for(var i = 1; i<=5; i++)
           {
-            if(i == 1)
-            {
-              fileUpload['certificate_'+i].upload   = true;
-              fileUpload['certificate_'+i].fileName = '';
-              setFileUpload({...fileUpload, ...fileUpload});
-            }
-            else
-            {
-              fileUpload['certificate_'+i].upload   = false;
-              fileUpload['certificate_'+i].fileName = '';
-              setFileUpload({...fileUpload, ...fileUpload});
-            }
+            fileUpload['certificate_'+i].upload   = true;
+            fileUpload['certificate_'+i].fileName = '';
+            setFileUpload({...fileUpload, ...fileUpload});
           }
         }
       }
@@ -166,12 +169,6 @@ function UploadCertificates(){
       let result = await response.json();
 
       if(result.success){
-        /*const nextElem = 'certificate_'+(index+1);
-        if(fileUpload[nextElem]){
-          fileUpload[nextElem].upload   = true;
-          fileUpload[nextElem].fileName = '';
-          setFileUpload({...fileUpload, ...fileUpload});
-        }*/
         getUploadedCertificates();
         alertContext.setAlertMessage({show:true, type: "success", message: result.msg});
       }
@@ -183,7 +180,7 @@ function UploadCertificates(){
     
   };
 
-  const deleteCertificate = async (fileName) => {
+  const deleteCertificate = async (fileName) => { 
 
     var decryptedLoginDetails = CryptoJS.AES.decrypt(localStorage.getItem('cred'), ENCYPTION_KEY);
     var loginDetails          = JSON.parse(decryptedLoginDetails.toString(CryptoJS.enc.Utf8));
@@ -195,9 +192,9 @@ function UploadCertificates(){
     jsonData["device_token"]      = DEVICE_TOKEN;
     jsonData["user_lat"]          = localStorage.getItem('latitude');
     jsonData["user_long"]         = localStorage.getItem('longitude');
-    jsonData["user_account_key"]  = loginDetails.account_key;
-    jsonData["user_account_type"] = loginDetails.account_type;
-    jsonData["file_name"]          = fileName;
+    jsonData["account_key"]       = loginDetails.account_key;
+    jsonData["account_type"]      = loginDetails.account_type;
+    jsonData["file_name"]         = fileName;
 
     const response = await fetch(`${API_URL}/deleteCertificate`, {
       method: "POST",
@@ -260,10 +257,10 @@ function UploadCertificates(){
           <div className="rounded jumbotron p-3 mt-3 mb-3">
             <form encType="multipart/form-data" className='choose-file'>
               {[...Array(MAX_CERTICATE_UPLOAD)].map((e, i) => 
-                <div key={i+1} className={`form-group brdr-btm parent ${(fileUpload['certificate_'+(i+1)].upload === true) ? '' : 'upload-disabled'}`}>
+                <div key={i+1} className={`form-group brdr-btm parent`}>
                   <input type="file" name={`certificate_${i+1}`} id={`certificate_${i+1}`} onChange={(event) => uploadCertificateChange(event, 'certificate_'+(i+1), i+1)}/>
                   <label>{(fileUpload['certificate_'+(i+1)].fileName === '') ? 'Upload Certificate '+(i+1) : fileUpload['certificate_'+(i+1)].fileName}</label>
-                  {(fileUpload['certificate_'+(i+1)].fileName !== '') && <span className="close float-end"><FontAwesomeIcon icon={faTrash} onClick={() => deleteCertificate(fileUpload['certificate_'+(i+1)].fileNames)}/></span>}
+                  {(fileUpload['certificate_'+(i+1)].fileName !== '') && <span className="close float-end"><FontAwesomeIcon icon={faTrash} onClick={() => deleteCertificate(fileUpload['certificate_'+(i+1)].fileName)}/></span>}
                 </div>
               )}
             </form>
