8f6fc28071e7c84d9dca2beeb8c8dde9675225ce
Manish Prasad <mprasad75@gmail.com>
2024-04-30 23:05:40 +0530

    Notification read

--- src/components/Notifications.js
+++ src/components/Notifications.js
@@ -23,7 +23,7 @@ function Notifications(){
   const [notificationList, setNotificationList] = useState([]);
   const [showModal, setShowModal] = useState(false); 
   const [editNotficationId, setEditNotificationId] = useState(0);
-  const [notificationDetails, setNotificationDetails] = useState('');
+  const [notificationDetails, setNotificationDetails] = useState({});
 
   const modalClose  = () => setShowModal(false);  
   const modalShow   = () => setShowModal(true);  
@@ -53,13 +53,6 @@ function Notifications(){
 
     let result = await response.json();
 
-    console.log(result);
-
-    /*var jsonValue = '{"success":true,"status":200,"msg":"My notifications data fetched","data":{"row":[{"thp_system_id":"0","sender_email_id":"sp-tech@serviceplace.org.in","sender_account_id":"1","sender_account_key":"THP@Ad321","receiver_email_id":"sab9195@gmail.com","receiver_mobile_no":"Sab","receiver_account_id":"101","receiver_account_key":"0up23298f7211","receiver_account_type":"3","email_template_key":"OTP_REGISTRATION_VERIFY","is_email_sent":"1","is_sms_sent":"1","custom_field":"test","custom_field_value":"test","notification_id":"14","device_type":"ANDROID","device_type_descr":"Android","device_token":"1232456","push_title":"OTP for Registration Verify at {{NGO_MAIL_SUBJECT_NAME}}","push_text":null,"push_img_url":null,"push_icon_url":"https:\/\/rgvn.serviceplace.org.in\/telehealth-uat\/public\/assets\/images\/default-push-icon-url.png","push_redirect_url":null,"sent_date":"2023-10-25 12:04:41+00","sent_date_descr":"2023-10-25 12:04","sent_status":"1","read_status":"1","read_date":null,"read_date_descr":null,"last_update":"2023-10-25 12:04"}],"numRows":1}}';
-
-    let result = JSON.parse(jsonValue);*/
-    
-    console.log(result);
     if(result.success){
       setNotificationList(result.data.row);
     }
@@ -88,17 +81,23 @@ function Notifications(){
       'notification_id': notificationId
     };
 
-    /*const response = await fetch(`${API_URL}/myNotifications`, {
+    const response = await fetch(`${API_URL}/myEachNotifications`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(jsonData)
-    });
+    })
 
-    let result = await response.json();*/
+    let result = await response.json();
+
+    setNotificationDetails(result.data.row[0]);
 
     modalShow();
+  }
+
+  const markAsRead = (notification_id) => {
+
   }
 
   return(
@@ -122,7 +121,7 @@ function Notifications(){
                 </div>
                 <div className='notification-time'>
                   {notification.last_update}
-                  <p>Yesterday</p>
+                  <p>{notification.days_ago}</p>
                 </div>
               </div>
             </div>
@@ -135,11 +134,11 @@ function Notifications(){
         </div>
         <Modal show={showModal} onHide={modalClose}>  
           <Modal.Body>  
-            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>  
+            <p>{notificationDetails.push_text}</p>  
           </Modal.Body>  
           <Modal.Footer className='justify-content-center'>  
             <Button variant="secondary" className='btn primary-bg-color text-light min-width-100 border-0' onClick={modalClose}>Close</Button>  
-            <Button variant="primary" className='btn primary-bg-color text-light border-0'>Mark as Read</Button>  
+            <Button variant="primary" className='btn primary-bg-color text-light border-0' onClick={() => markAsRead(notificationDetails.notification_id)}>Mark as Read</Button>  
           </Modal.Footer>  
         </Modal>
       </div>
