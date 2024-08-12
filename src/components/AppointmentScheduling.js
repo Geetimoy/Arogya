4a7f7a6aa6dc4bb2aa08e6429ccb89c7971e410c
GeetimoyOfficePC145 <geetimoy.sahu@gmail.com>
2024-08-12 17:17:03 +0530

    Appointment schedule listing

--- src/components/AppointmentScheduling.js
+++ src/components/AppointmentScheduling.js
@@ -64,7 +64,34 @@ function AppointmentScheduling(){
         </div>
       </div>
       <div className="app-body bookings">
-        <div className="add-booking mb-3"><a class="btn btn-sm btn-primary primary-bg-color border-0" href="create-schedule">Add/Update your Schedule</a></div>
+        <div className="add-booking mb-3">
+          <a class="btn btn-sm btn-primary primary-bg-color border-0" href="create-schedule">Add/Update your Schedule</a>
+        </div>
+        <div className="row">
+          <div className="col-12">
+            <div className="button-box pos-relative mb-3">
+              <p><span className="d-block">Doctor Name:</span> Dr. D Sinha</p>
+              <p><span className="d-block">Specialization:</span> Heart</p>
+              <p><span className="d-block">Schedule Type :</span> Single</p>
+              <p><span className="d-block">Appointment Date:</span> Tuesday 6th August, 2024</p>
+              <p><span className="d-block">Appointment Time:</span> 04:00 PM - 07:00PM</p>
+              <p><span className="d-block">Place:</span> New Life - Bablatala</p>
+              <p><span className="d-block">Consultation Mode:</span> Offline (Clinic)</p>
+              <div class="mb-3 mt-3 text-center"><button type="submit" class="btn primary-bg-color text-light">Book Now</button></div>
+            </div>
+            <div className="button-box mb-3">
+              <span className="float-end"> <FontAwesomeIcon icon={faEllipsisV} /> </span>
+              <p><span className="d-block">Doctor Name:</span> Dr.NP Kar</p>
+              <p><span className="d-block">Specialization:</span> Medicine</p>
+              <p><span className="d-block">Schedule Type :</span> Single</p>
+              <p><span className="d-block">Appointment Date:</span> Monday 12th August, 2024</p>
+              <p><span className="d-block">Appointment Time:</span> 07:00 PM - 10:00PM</p>
+              <p><span className="d-block">Place:</span> Apex - Joramandir</p>
+              <p><span className="d-block">Consultation Mode:</span> Offline (Clinic)</p>
+              <div class="mb-3 mt-3 text-center"><button type="submit" class="btn primary-bg-color text-light">Book Now</button></div>
+            </div>
+          </div>
+        </div>
         
         {/* <div className="row">
           <div className="col-12">
