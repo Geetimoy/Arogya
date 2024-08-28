891eaab691c10984149552a74637431930d941c9
GeetimoyOfficePC145 <geetimoy.sahu@gmail.com>
2024-08-28 21:26:12 +0530

    appointment schedule

---
+++ src/components/AppointmentSchedulingVolunteer.js
@@ -0,0 +1,121 @@
+import { useState, useContext, useEffect } from 'react';
+import Appfooter from "./AppFooter";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEllipsisV, faLongArrowAltLeft, faBell } from '@fortawesome/free-solid-svg-icons';
+
+import { Link, useNavigate } from "react-router-dom";
+
+import SystemContext from "../context/system/SystemContext";
+
+import "./Bookings.css";
+
+function AppointmentSchedulingVolunteer(){
+
+  const systemContext = useContext(SystemContext);
+
+  const [isActive, setIsActive] = useState(false);
+
+  const handleClick = () => {
+    setIsActive(!isActive); // Toggle the state
+  };
+
+  const [isMActive, setIsMActive] = useState(false);
+
+  const handle2Click = () => {
+    setIsMActive(!isMActive); // Toggle the state
+  };
+
+  return(
+    <>
+      <div className='app-top inner-app-top services-app-top'>
+        <div className='app-top-box d-flex align-items-center justify-content-between'>
+          <div className='app-top-left d-flex align-items-center'>
+            <div className='scroll-back'>
+              <Link to="/services" className=''>
+                <FontAwesomeIcon icon={faLongArrowAltLeft} />
+              </Link>
+            </div>
+            <h5 className='mx-2 mb-0'>Appointment Schedules</h5>
+          </div>
+          <div className='app-top-right d-flex'> 
+            <div className='position-relative'>
+              <Link to="/notifications">
+              <FontAwesomeIcon icon={faBell}  className='mx-3'/> 
+              <span className='top-header-notification primary-bg-color'>3</span>
+              </Link>
+            </div> 
+            <div className={`my-element2 ${isMActive ? 'active' : ''}`} onClick={handle2Click}><FontAwesomeIcon icon={faEllipsisV} /></div>
+            <div className='drop-menu'>
+                <ul>
+                  <li><Link to={"/aboutserviceplace"}>About Service Place</Link></li>
+                  {
+                    (systemContext.systemDetails.thp_system_id !== 0) && <li><Link to={"/about-ngo"}>About {systemContext.systemDetails.thp_system_name}</Link></li>
+                  }
+                  <li><Link to={"/contactus"}>Contact Us</Link></li>
+                  <li><Link to={"/feedback"}>Feedback</Link></li>
+                  <li><Link to={"/help"}>Help</Link></li>
+                  <li><Link to={"/logout"}>Logout</Link></li>
+                </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="app-body bookings">
+        <div className="d-flex justify-content-between mb-3">
+          <div className='d-flex advaced-search btn btn-sm btn-primary primary-bg-color border-0'>
+            
+            <span className="search-ultra-plus"> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 330 330">
+              <path d="M325.606 304.394 223.328 202.117c16.707-21.256 26.683-48.041 26.683-77.111C250.011 56.077 193.934 0 125.005 0 56.077 0 0 56.077 0 125.006 0 193.933 56.077 250.01 125.005 250.01c29.07 0 55.855-9.975 77.11-26.681l102.278 102.277c2.929 2.93 6.768 4.394 10.607 4.394s7.678-1.464 10.606-4.394c5.859-5.857 5.859-15.355 0-21.212zM30 125.006C30 72.619 72.619 30 125.005 30c52.387 0 95.006 42.619 95.006 95.005 0 52.386-42.619 95.004-95.006 95.004C72.619 220.01 30 177.391 30 125.006z"></path>
+
+            </svg> </span>
+            <span>Advanced Search</span>
+          </div>
+          <span id="filters-search-options-toggle" class="btn p-0">
+            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"> <path d="M2.5 20c-0.276 0-0.5-0.224-0.5-0.5v-8c0-0.276 0.224-0.5 0.5-0.5s0.5 0.224 0.5 0.5v8c0 0.276-0.224 0.5-0.5 0.5z" fill="#333"></path> <path d="M2.5 6c-0.276 0-0.5-0.224-0.5-0.5v-5c0-0.276 0.224-0.5 0.5-0.5s0.5 0.224 0.5 0.5v5c0 0.276-0.224 0.5-0.5 0.5z" fill="#333"></path> <path d="M3.5 10h-2c-0.827 0-1.5-0.673-1.5-1.5s0.673-1.5 1.5-1.5h2c0.827 0 1.5 0.673 1.5 1.5s-0.673 1.5-1.5 1.5zM1.5 8c-0.276 0-0.5 0.224-0.5 0.5s0.224 0.5 0.5 0.5h2c0.276 0 0.5-0.224 0.5-0.5s-0.224-0.5-0.5-0.5h-2z" fill="#333"></path> <path d="M9.5 20c-0.276 0-0.5-0.224-0.5-0.5v-4c0-0.276 0.224-0.5 0.5-0.5s0.5 0.224 0.5 0.5v4c0 0.276-0.224 0.5-0.5 0.5z" fill="#333"></path> <path d="M9.5 10c-0.276 0-0.5-0.224-0.5-0.5v-9c0-0.276 0.224-0.5 0.5-0.5s0.5 0.224 0.5 0.5v9c0 0.276-0.224 0.5-0.5 0.5z" fill="#333"></path> <path d="M10.5 14h-2c-0.827 0-1.5-0.673-1.5-1.5s0.673-1.5 1.5-1.5h2c0.827 0 1.5 0.673 1.5 1.5s-0.673 1.5-1.5 1.5zM8.5 12c-0.276 0-0.5 0.224-0.5 0.5s0.224 0.5 0.5 0.5h2c0.276 0 0.5-0.224 0.5-0.5s-0.224-0.5-0.5-0.5h-2z" fill="#333"></path> <path d="M16.5 20c-0.276 0-0.5-0.224-0.5-0.5v-10c0-0.276 0.224-0.5 0.5-0.5s0.5 0.224 0.5 0.5v10c0 0.276-0.224 0.5-0.5 0.5z" fill="#333"></path> <path d="M16.5 4c-0.276 0-0.5-0.224-0.5-0.5v-3c0-0.276 0.224-0.5 0.5-0.5s0.5 0.224 0.5 0.5v3c0 0.276-0.224 0.5-0.5 0.5z" fill="#333"></path> <path d="M17.5 8h-2c-0.827 0-1.5-0.673-1.5-1.5s0.673-1.5 1.5-1.5h2c0.827 0 1.5 0.673 1.5 1.5s-0.673 1.5-1.5 1.5zM15.5 6c-0.276 0-0.5 0.224-0.5 0.5s0.224 0.5 0.5 0.5h2c0.276 0 0.5-0.224 0.5-0.5s-0.224-0.5-0.5-0.5h-2z" fill="#333"></path> </svg>
+          </span>
+        </div>
+      <div className="row">
+          <div className="col-12">
+            <div className="button-box pos-relative mb-3">
+              {/* <div className={`three-dot my-element2 ${isActive ? 'active' : ''}`} onClick={handleClick}><FontAwesomeIcon icon={faEllipsisV} /></div> */}
+              {/* <div className='drop-menu'>
+                <ul>
+                  <li><Link to={"/upload-prescription"}>Upload Prescription</Link></li>
+                  <li><Link to={"/testreports"}>Upload Test Reports</Link></li>
+                  <li><Link to={"#"}>Cancel Booking</Link></li>
+                  <li><Link to={"#"}>Close Booking</Link></li>
+                  <li><Link to={"/view-review"}>View Review</Link></li>
+                  <li><Link to={"/write-review"}>Write Review</Link></li>
+                </ul>
+              </div> */}
+              <p><span className="d-block">Doctor Name:</span> RM Das</p>
+              <p><span className="d-block">Specialization:</span> Heart</p>
+              <p><span className="d-block">Schedule Type :</span> Single</p>
+              <p><span className="d-block">Appointment Date:</span> Tuesday 30th August, 2024</p>
+              <p><span className="d-block">Appointment Time:</span> 04:00 PM - 07:00PM</p>
+              <p><span className="d-block">Place:</span> New Life - Bablatala</p>
+              <p><span className="d-block">Consultation Mode:</span> Offline (Clinic)</p>
+              <p><span className="d-block">Booking Status:</span> Doctor Confirmation Pending</p>
+            </div>
+            <div className="button-box mb-3">
+              <span className="float-end"> <FontAwesomeIcon icon={faEllipsisV} /> </span>
+              <p><span className="d-block">Doctor Name:</span> A Acharya</p>
+              <p><span className="d-block">Specialization:</span> General Physician</p>
+              <p><span className="d-block">Schedule Type :</span> Single</p>
+              <p><span className="d-block">Appointment Date:</span> Monday 12th Sept, 2024</p>
+              <p><span className="d-block">Appointment Time:</span> 07:00 PM - 10:00PM</p>
+              <p><span className="d-block">Place:</span> Apex - Joramandir</p>
+              <p><span className="d-block">Consultation Mode:</span> Offline (Clinic)</p>
+              <p><span className="d-block">Booking Status:</span> Doctor Confirmation Pending</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Appfooter></Appfooter>
+    </>
+  )
+}
+
+
+export default AppointmentSchedulingVolunteer;
