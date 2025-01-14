9948ff14fb2f3542d49bb7cae6fec8df5ca5b987
Manish Prasad <mprasad75@gmail.com>
2025-01-14 21:54:53 +0530

    Worked for elder persoan

--- src/components/elderpersons/CreateElderPerson.js
+++ src/components/elderpersons/CreateElderPerson.js
@@ -1,4 +1,5 @@
 import { useState, useContext, useEffect } from 'react';
+import CryptoJS from "crypto-js";
 import Appfooter from "../AppFooter";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -7,13 +8,17 @@ import { faSearch, faEllipsisV, faBell, faLongArrowAltLeft } from '@fortawesome/
 import { Link } from "react-router-dom";
 
 import SystemContext from "../../context/system/SystemContext";
+import AlertContext from '../../context/alert/AlertContext';
 
 import  './ElderPersons.css';
 import Select from 'react-select';
 
+import { API_URL, ENCYPTION_KEY, DEVICE_TYPE, DEVICE_TOKEN } from "../util/Constants";
+
 function CreateElderPerson(){
 
   const systemContext = useContext(SystemContext);
+  const alertContext  = useContext(AlertContext);
 
   const [isMActive, setIsMActive] = useState(false);
   
@@ -28,6 +33,156 @@ function CreateElderPerson(){
     { label: 'Umananda Temple, Guwahati', value: '3' },
     { label: 'Morigaon', value: '4' },
   ];
+
+  const handleChange1 = (values) => {
+    var selectedArea = [];
+    if(values.length > 0){
+      values.forEach((item, index) => {
+        selectedArea.push(item.value);
+      })
+    }
+    if(selectedArea.length > 0){
+      setFormData({...formData, ['patient_service_area']: {...formData['patient_service_area'], value:selectedArea.join(), errorClass:"", errorMessage:""}});
+    }
+    else{
+      setFormData({...formData, ['patient_service_area']: {...formData['patient_service_area'], value:"", errorClass:"form-error", errorMessage:"This field is required!"}});
+    }
+    setSelectedOptions(values);
+  };
+
+  const [formData, setFormData] = useState({
+    elder_name: {required: true, value:"", errorClass:"", errorMessage:""},
+    elder_guardian: {required: true, value:"", errorClass:"", errorMessage:""},
+    elder_occupation: {required: true, value:"", errorClass:"", errorMessage:""},
+    elder_gender: {required: true, value:"male", errorClass:"", errorMessage:""},
+    elder_age: {required: true, value:"", errorClass:"", errorMessage:""},
+    is_personal_mobile_number: {required: true, value:"t", errorClass:"", errorMessage:""},
+    elder_phone_no: {required: true, value:"", errorClass:"", errorMessage:""},
+    elder_whatsapp_no: {required: false, value:"", errorClass:"", errorMessage:""},
+    elder_email: {required: false, value:"", errorClass:"", errorMessage:""},
+    elder_address: {required: true, value:"", errorClass:"", errorMessage:""},
+    elder_address_2: {required: false, value:"", errorClass:"", errorMessage:""},
+    elder_landmark: {required: false, value:"", errorClass:"", errorMessage:""},
+    elder_city: {required: true, value:"", errorClass:"", errorMessage:""},
+    elder_state: {required: true, value:"", errorClass:"", errorMessage:""},
+    elder_pincode: {required: true, value:"", errorClass:"", errorMessage:""},
+    elder_service_area: {required: true, value:"", errorClass:"", errorMessage:""},
+    elder_education: {required: true, value:"", errorClass:"", errorMessage:""},
+    elder_sub_volunteer: {required: false, value:"", errorClass:"", errorMessage:""},
+    elder_special_notes: {required: false, value:"", errorClass:"", errorMessage:""}
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    if(value.trim() !== ""){
+      setFormData({...formData, [name]: {...formData[name], value:value, errorClass:"", errorMessage:""}});
+    }
+    else{
+      setFormData({...formData, [name]: {...formData[name], value:value, errorClass:"form-error", errorMessage:"This field is required!"}});
+    }
+  }
+
+  const resetForm = () => {
+    const fieldName = Object.keys(formData);
+    setSelectedOptions([]);
+    fieldName.forEach((element) => {
+      if(element === "gender"){
+        formData[element].value         = "male";
+        formData[element].errorClass    = "";
+        formData[element].errorMessage  = "";
+      }
+      else{
+        formData[element].value         = "";
+        formData[element].errorClass    = "";
+        formData[element].errorMessage  = "";
+      }
+    })
+    setFormData({...formData, ...formData});
+  }
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault(); 
+    let errorCounter = validateForm();console.log(formData);
+    if(errorCounter === 0){
+
+      var decryptedLoginDetails = JSON.parse(CryptoJS.AES.decrypt(localStorage.getItem("cred"), ENCYPTION_KEY).toString(CryptoJS.enc.Utf8));
+
+      let jsonData = {};
+      jsonData['system_id']                 = systemContext.systemDetails.system_id;
+      jsonData["introducer_account_key"]    = decryptedLoginDetails.account_key;
+      jsonData["introducer_account_type"]   = decryptedLoginDetails.account_type;
+      jsonData["user_login_id"]             = decryptedLoginDetails.login_id;
+      jsonData["device_type"]               = DEVICE_TYPE; //getDeviceType();
+      jsonData["device_token"]              = DEVICE_TOKEN;
+      jsonData["user_lat"]                  = localStorage.getItem('latitude');
+      jsonData["user_long"]                 = localStorage.getItem('longitude');
+
+      var serviceArea                       = '{'+formData['patient_service_area'].value+'}';
+
+      jsonData["patient_name"]              = formData['patient_name'].value;
+      jsonData["patient_father_name"]       = formData['patient_father_name'].value;
+      jsonData["patient_contact_number"]    = formData['patient_phone_no'].value;
+      jsonData["patient_whatsup_number"]    = formData['patient_whatsapp_no'].value;
+      jsonData["patient_email_id"]          = formData['patient_email'].value;
+      jsonData["patient_age"]               = formData['patient_age'].value;
+      jsonData["patient_gender"]            = formData['patient_gender'].value;
+      jsonData["patient_address"]           = formData['patient_address'].value;
+      jsonData["patient_address_2"]         = formData['patient_address_2'].value;
+      jsonData["patient_state"]             = formData['patient_state'].value;
+      jsonData["patient_postal_code"]       = formData['patient_pincode'].value;
+      jsonData["patient_landmark"]          = formData['patient_landmark'].value;
+      jsonData["patient_city"]              = formData['patient_city'].value;
+      jsonData["is_bpl"]                    = formData['patient_is_bpl'].value;
+      jsonData["is_your_personal_number"]   = formData['is_personal_mobile_number'].value;
+      jsonData["patient_education"]         = formData['patient_education'].value;
+      jsonData["special_note"]              = formData['patient_special_notes'].value;
+      jsonData["service_area"]              = serviceArea;
+
+      const response = await fetch(`${API_URL}/addUpdatePatientProfile`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(jsonData),
+      });
+      
+      let result = await response.json();
+
+      if(result.success){
+        alertContext.setAlertMessage({show:true, type: "success", message: result.msg});
+        resetForm();
+      }
+      else{
+        alertContext.setAlertMessage({show:true, type: "error", message: result.msg});
+      }
+
+    }
+  }
+
+  const validateForm = () => {
+    const fieldName = Object.keys(formData);
+    let errorCounter = 0;
+    fieldName.forEach((element) => {
+      if(formData[element].required && (formData[element].value === "" || formData[element].value === null)){
+        formData[element].errorMessage = "This field is required!";
+        formData[element].errorClass = "form-error";
+        errorCounter++;
+      }
+      else{
+        var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+        if((element === "patient_email") && (formData[element].value.trim() !== "") && (!formData[element].value.match(validRegex))){
+          formData[element].errorMessage = "Please enter a valid email!";
+          formData[element].errorClass = "form-error";
+        }
+        else{
+          formData[element].errorMessage = "";
+          formData[element].errorClass = "";
+        }
+      }
+    })
+    setFormData({...formData, ...formData});
+    return errorCounter;
+  }
 
   return(
     <>
