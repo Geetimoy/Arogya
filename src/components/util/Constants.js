517ffe6fdc4e204ecbfc34999a2ff151af2be7f4
Manish Prasad <mprasad75@gmail.com>
2025-04-03 22:13:43 +0530

    Added  total Limit

--- src/components/util/Constants.js
+++ src/components/util/Constants.js
@@ -4,3 +4,4 @@ export const ENCYPTION_KEY = "arogya#0987654321";
 export const DEVICE_TYPE = "ANDROID";
 export const DEVICE_TOKEN = "fpDnSllATPC2-RQURuALJy:APA91bHjjUGG7g88GHiu2qz1Gm9PCjCbEQvOTFpU74Ffcwt9JPSi-03eLb-LqWmKhJG2TZgZGaUuolEU7G_IBVKoo2OLf5Ge_KwsMpJ0_g0lFYnR5TjgoCTTokp9s-IpaoX1AUMI-hMu";
 export const MAX_CERTICATE_UPLOAD = 5;
+export const PAGINATION_LIMIT = 10;
