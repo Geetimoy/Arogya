93a2cd8ca40c65a1dd4bb6274f108b5bf3dd255f
Manish Prasad <mprasad75@gmail.com>
2025-06-25 22:50:11 +0530

    display name

--- src/components/childmalnutrition/ChildPeriodicData.js
+++ src/components/childmalnutrition/ChildPeriodicData.js
@@ -46,6 +46,7 @@ function ChildPeriodicData(){
 
   const [remarks, setRemarks] = useState(''); 
   const [periodicList, setPeriodicList] = useState([]); 
+  const [userDetails, setUserDetails] = useState([]);
   const [urlParam, setUrlParam] = useState(useParams());
   const editAccountKey = urlParam.accountKey;
 
@@ -251,6 +252,46 @@ function ChildPeriodicData(){
 
   }
 
+  const getProfileDetails = async () => {
+
+    var decryptedLoginDetails = JSON.parse(CryptoJS.AES.decrypt(localStorage.getItem("cred"), ENCYPTION_KEY).toString(CryptoJS.enc.Utf8));
+    
+    let jsonData = {};
+
+    jsonData['system_id']                 = systemContext.systemDetails.system_id;
+    jsonData["account_type"]              = 31;
+    jsonData["account_key"]               = editAccountKey;
+    jsonData["user_login_id"]             = decryptedLoginDetails.login_id;
+    jsonData["volunteer_account_key"]     = decryptedLoginDetails.account_key;
+    jsonData["device_type"]               = DEVICE_TYPE; //getDeviceType();
+    jsonData["device_token"]              = DEVICE_TOKEN;
+    jsonData["user_lat"]                  = localStorage.getItem('latitude');
+    jsonData["user_long"]                 = localStorage.getItem('longitude');
+    
+    const response1 = await fetch(`${API_URL}/getProfileDetails`, {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json",
+        },
+        body: JSON.stringify(jsonData),
+    });
+    let result = await response1.json();
+
+    if(result.success){
+      setUserDetails(result.data);
+    }
+    else{
+      setUserDetails([]); 
+    }
+  }
+
+  useEffect(() => {
+    if(systemContext.systemDetails.system_id && editAccountKey){
+      getProfileDetails();
+    }
+    // eslint-disable-next-line
+  }, [systemContext.systemDetails.system_id, editAccountKey]);
+
   return(
     <>
       <div className='app-top inner-app-top services-app-top'>
@@ -284,7 +325,9 @@ function ChildPeriodicData(){
       <div className='app-body form-all upadte-periodic-data'>
         <p><small>Update Child Periodic Data</small></p>
         <form className="mt-3" name="periodicDataForm" id="periodicDataForm" onSubmit={handleFormSubmit}>
-          <div className="position-absolute mt-1 child-details">Test Child, M, 12yrs</div>
+          <div className="position-absolute mt-1 child-details">
+            {(userDetails.display_name) && <span className="text-muted">({userDetails.display_name}, {userDetails.gender}, {userDetails.age})</span>}
+          </div>
           <div className='mb-3 mt-3 text-end'>
             <button type="button" className='btn btn-sm primary-bg-color text-light' onClick={onAddBtnClick}>Add More Category</button>
           </div>
