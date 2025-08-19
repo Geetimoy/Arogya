95d40dfbc32e516a97bba1a67ae000b1f9faf50f
GeetimoyOfficePC145 <geetimoy.sahu@gmail.com>
2025-08-19 21:48:10 +0530

    MyConsent

--- src/components/MyConsent.js
+++ src/components/MyConsent.js
@@ -12,6 +12,7 @@ function MyConsent(){
         <div class="custom-control custom-radio custom-control-inline mt-2"><input type="radio" id="save_info" name="consent" class="custom-control-input" value="t" checked /><label class="custom-control-label no-style" for="save_info">Yes, save my information </label></div>
         <div class="custom-control custom-radio custom-control-inline mt-2"><input type="radio" id="withdraw_consent" name="consent" class="custom-control-input" value="f" /><label class="custom-control-label no-style" for="withdraw_consent">Withdraw my consent </label></div>
       </div>
+      <div class="btns-group d-flex justify-content-center"><button type="submit" class="btn btn-primary primary-bg-color border-0 mx-2">Save</button></div>
     </div>
     <Appfooter></Appfooter>
     </>
