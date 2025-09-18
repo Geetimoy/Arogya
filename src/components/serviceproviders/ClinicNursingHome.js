daa8f3d6212465930354c76852c1f2716d32c0b1
GeetimoyOfficePC145 <geetimoy.sahu@gmail.com>
2025-09-18 22:53:40 +0530

    Clinic Nursinghome

--- src/components/serviceproviders/ClinicNursingHome.js
+++ src/components/serviceproviders/ClinicNursingHome.js
@@ -1,23 +1,86 @@
 import { useState, useContext, useEffect } from 'react';
+import CryptoJS from "crypto-js";
 import Appfooter from "../AppFooter";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEllipsisV, faLongArrowAltLeft, faSearch } from '@fortawesome/free-solid-svg-icons';
 import { Link } from "react-router-dom";
 
+import { API_URL, ENCYPTION_KEY, DEVICE_TYPE, DEVICE_TOKEN } from "../util/Constants";
+
 import './ServiceProviders.css'
 import AppTopNotifications from '../AppTopNotifications';
 import SystemContext from "../../context/system/SystemContext";
+import AlertContext from '../../context/alert/AlertContext';
 
 function ClinicNursingHome() {
 
   const systemContext = useContext(SystemContext);
-  
+  const alertContext = useContext(AlertContext);
+
      const [isMActive, setIsMActive] = useState(false);
   
      const handle2Click = () => {
       setIsMActive(!isMActive); // Toggle the state
     };
+
+      const [providerId, setProviderId] = useState('');
+      const [providerList, setProviderList] = useState([]);
+      const listClinicNursingHome = async (searchKey) => {
+    
+        var decryptedLoginDetails = JSON.parse(CryptoJS.AES.decrypt(localStorage.getItem("cred"), ENCYPTION_KEY).toString(CryptoJS.enc.Utf8));
+    
+        let jsonData = {};
+        jsonData['system_id']                 = systemContext.systemDetails.system_id;
+        jsonData["volunteer_account_type"]    = decryptedLoginDetails.account_type;
+        jsonData["volunteer_account_key"]     = decryptedLoginDetails.account_key;
+        // jsonData["user_login_id"]             = decryptedLoginDetails.login_id;
+        jsonData["device_type"]               = DEVICE_TYPE; //getDeviceType();
+        jsonData["device_token"]              = DEVICE_TOKEN;
+        jsonData["user_lat"]                  = localStorage.getItem('latitude');
+        jsonData["user_long"]                 = localStorage.getItem('longitude');
+        jsonData["provider_id"]               = providerId; // 1 - Clinic Nursing Home
+        jsonData["search_param"]              = {
+                                                  "by_keywords": searchKey,
+                                                  "limit": "0",
+                                                  "offset": "0",
+                                                  "order_by_field": "appointment_id",
+                                                  "order_by_value": "desc"
+                                                }
+    
+        const response = await fetch(`${API_URL}/getServiceProviderDetails`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(jsonData),
+        });
+    
+        let result = await response.json();
+        console.log(result);
+        
+        // if(result.success){
+        //   alertContext.setAlertMessage({show:true, type: "success", message: result.msg});
+        // }
+        // else{
+        //   alertContext.setAlertMessage({show:true, type: "error", message: result.msg});
+        // }
+
+        if(result.success){
+          setProviderList(result.data.results);
+        }
+        else{
+          setProviderList([]); 
+        }
+    
+      }
+
+      useEffect(() => {
+          if(systemContext.systemDetails.system_id){
+            listClinicNursingHome("");
+          }
+          // eslint-disable-next-line
+        }, [systemContext.systemDetails.system_id]);
 
   return (
     <>
@@ -33,7 +96,9 @@ function ClinicNursingHome() {
         </div>
         <div className='app-top-right d-flex'> 
           <AppTopNotifications /> 
-          <div className={`my-element2 ${isMActive ? 'active' : ''}`} onClick={handle2Click}><FontAwesomeIcon icon={faEllipsisV} /></div>
+          <div className={`my-element2 ${isMActive ? 'active' : ''}`} onClick={handle2Click}>
+            <FontAwesomeIcon icon={faEllipsisV} />
+          </div>
           <div className='drop-menu'>
               <ul>
                 <li><Link to={"/aboutserviceplace"}>About Service Place</Link></li>
@@ -57,7 +122,7 @@ function ClinicNursingHome() {
         </div>
       </div>
       <div className='row'>
-        <div className='col-12 mb-3'>
+        {/* <div className='col-12 mb-3'>
           <div className='button-box-providers'>
             <ul className='p-0 m-0'>
               <li><span className='fw-bold min-width'>Name :</span> Samrat Nursing Home</li>
@@ -66,8 +131,20 @@ function ClinicNursingHome() {
               <li><span className='fw-bold'>Services :</span> General Checkup, Vaccination</li>
             </ul>
           </div>
-        </div>
+        </div> */}
         <div className='col-12 mb-3'>
+          {providerList.map((provider, index) => (
+          <div className='button-box-providers'>
+            <ul className='p-0 m-0'>
+              <li><span className='fw-bold min-width'>Name :</span> {provider.provider_name}</li>
+              <li><span className='fw-bold'>Location :</span> {provider.location}</li>
+              <li><span className='fw-bold'>Contact :</span> {provider.contact}</li>
+              <li><span className='fw-bold'>Services :</span> {provider.services.join(", ")}</li>
+            </ul>
+          </div>
+           ))}
+        </div>
+        {/* <div className='col-12 mb-3'>
           <div className='button-box-providers'>
             <ul className='p-0 m-0'>
               <li><span className='fw-bold min-width'>Name :</span> Samrat Nursing Home</li>
@@ -76,17 +153,7 @@ function ClinicNursingHome() {
               <li><span className='fw-bold'>Services :</span> General Checkup, Vaccination</li>
             </ul>
           </div>
-        </div>
-        <div className='col-12 mb-3'>
-          <div className='button-box-providers'>
-            <ul className='p-0 m-0'>
-              <li><span className='fw-bold min-width'>Name :</span> Samrat Nursing Home</li>
-              <li><span className='fw-bold'>Location :</span> Baguiati, Kolkata</li>
-              <li><span className='fw-bold'>Contact :</span> 1234567890</li>
-              <li><span className='fw-bold'>Services :</span> General Checkup, Vaccination</li>
-            </ul>
-          </div>
-        </div>
+        </div> */}
       </div>
     </div>
     <Appfooter></Appfooter>
