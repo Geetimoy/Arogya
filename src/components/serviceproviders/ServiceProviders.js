d38d98278545d7063f6add1f65abe46bee069249
Manish Prasad <mprasad75@gmail.com>
2025-09-20 22:29:07 +0530

    Worked for Service Provider categpry

--- src/components/serviceproviders/ServiceProviders.js
+++ src/components/serviceproviders/ServiceProviders.js
@@ -1,4 +1,5 @@
 import { useState, useContext, useEffect } from 'react';
+import CryptoJS from "crypto-js";
 import Appfooter from "../AppFooter";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -7,6 +8,8 @@ import { Link } from "react-router-dom";
 
 import AppTopNotifications from '../AppTopNotifications';
 import SystemContext from "../../context/system/SystemContext";
+
+import { API_URL, ENCYPTION_KEY, DEVICE_TYPE, DEVICE_TOKEN, PAGINATION_LIMIT } from "../util/Constants";
 
 
 import nursinghome from '../../assets/images/nursing-home.png';
@@ -20,13 +23,76 @@ import './ServiceProviders.css'
 
 function ServiceProviders(){
 
-   const systemContext = useContext(SystemContext);
+  const systemContext = useContext(SystemContext);
 
-   const [isMActive, setIsMActive] = useState(false);
+  const [isMActive, setIsMActive] = useState(false);
 
-   const handle2Click = () => {
+  const handle2Click = () => {
     setIsMActive(!isMActive); // Toggle the state
   };
+
+  const [providerCategory, setProviderCategory] = useState([]);
+
+  const fetchProviderCategory = async () => {
+
+    var decryptedLoginDetails = JSON.parse(CryptoJS.AES.decrypt(localStorage.getItem("cred"), ENCYPTION_KEY).toString(CryptoJS.enc.Utf8));
+
+    let jsonData = {};
+    jsonData['system_id']                 = systemContext.systemDetails.system_id;
+    jsonData["volunteer_account_type"]    = decryptedLoginDetails.account_type;
+    jsonData["volunteer_account_key"]     = decryptedLoginDetails.account_key;
+    jsonData["device_type"]               = DEVICE_TYPE; //getDeviceType();
+    jsonData["device_token"]              = DEVICE_TOKEN;
+    jsonData["user_lat"]                  = localStorage.getItem('latitude');
+    jsonData["user_long"]                 = localStorage.getItem('longitude');
+
+    const response = await fetch(`${API_URL}/getServiceProviderCategory`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(jsonData),
+    });
+
+    let result = await response.json();
+
+    if(result.success && result.data.length > 0){
+      let resultData = result.data.map((category, index) => {
+        if(category.id == '1') {
+          category.url = '/clinic-nursing-home';
+        }
+        if(category.id == '2') {
+          category.url = '/pathology';
+        }
+        if(category.id == '3') {
+          category.url = '/pharmacy';
+        }
+        if(category.id == '4') {
+          category.url = '/nurse-care';
+        }
+        if(category.id == '10') {
+          category.url = '/home-care';
+        }
+        if(category.id == '9') {
+          category.url = '/prefered-hospital';
+        }
+
+        return category;
+      })
+      setProviderCategory(resultData); // Append new data to existing list
+    }
+    else{
+      setProviderCategory([]); 
+    }
+    
+  }
+
+  useEffect(() => {
+    if(systemContext.systemDetails.system_id){ 
+      fetchProviderCategory();
+    }
+    // eslint-disable-next-line
+  }, [systemContext.systemDetails.system_id]);
 
   return(
     <>
@@ -60,7 +126,7 @@ function ServiceProviders(){
     </div>
     <div className="app-body service-provider">
       <div className='row'>
-        <div className='col-6 mb-3'>
+        {/* <div className='col-6 mb-3'>
           <div className='button-box'>
             <Link to="/prefered-hospital"><img src={hospital} alt='' />
             <h6 className='mb-0'>Prefered Hospital</h6></Link>
@@ -97,9 +163,21 @@ function ServiceProviders(){
           <div className='button-box'>
             <Link to="/home-care"><img src={aaya} alt='' />
             <h6 className='mb-0'>In Home Care</h6></Link>
-            
           </div>
-        </div>
+        </div> */}
+
+        {
+          providerCategory && providerCategory.map((category, index) => (
+            <div className='col-6 mb-3' key={category.id}>
+              <div className='button-box'>
+                <Link to={category.url}><img src={category.category_icon} alt='' />
+                <h6 className='mb-0'>{category.category_name}</h6></Link>
+              </div>
+            </div>
+          ))
+        }
+
+
       </div>
     </div>
     <Appfooter></Appfooter>
