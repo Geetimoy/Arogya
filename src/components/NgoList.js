27986cab454ab1da69b35b683cb85515a5aef543
GeetimoyOfficePC145 <geetimoy.sahu@gmail.com>
2026-06-07 12:09:55 +0530

    Blood Group

--- src/components/NgoList.js
+++ src/components/NgoList.js
@@ -11,6 +11,7 @@ import rgvnlogo from '../assets/images/rgvn-icon.png';
 import b2hlogo from '../assets/images/b2h-icon.png';
 import ukhralogo from '../assets/images/ukhra-icon.png';
 import sevaapurulialogo from '../assets/images/sevaapurulia-icon.png';
+import motinagarlogo from '../assets/images/motinagar-icon.png';
 
 function NgoList(){
   return(
@@ -24,48 +25,56 @@ function NgoList(){
             <span className="m-2">NGO List</span>
           </div>
           <div className="ngolist-box">
-          <div className="row">
-          <div className="col-6">
-            <div className="button-box">
-              <Link to="/login">
-                <img src={globallogo} alt='' />
-                <h6>For Anywhere</h6>
-              </Link>
+            <div className="row">
+              <div className="col-6">
+                <div className="button-box">
+                  <Link to="/login">
+                    <img src={globallogo} alt='' />
+                    <h6>For Anywhere</h6>
+                  </Link>
+                </div>
+              </div>
+              <div className="col-6">
+                <div className="button-box">
+                  <Link to="https://b2happ.serviceplace.org.in/" target="_blank">
+                    <img src={b2hlogo} alt='' />
+                    <h6>Near Kharagpur City</h6>
+                  </Link>
+                </div>
+              </div>
+              <div className="col-6">
+                <div className="button-box">
+                  <Link to="https://rgvnapp.serviceplace.org.in/" target="_blank">
+                    <img src={rgvnlogo} alt='' />
+                    <h6>Near Morigaon, Assam</h6>
+                  </Link>
+                </div>
+              </div>
+              <div className="col-6">
+                <div className="button-box">
+                  <Link to="https://ukhraapp.serviceplace.org.in/" target="_blank">
+                    <img src={ukhralogo} alt='' />
+                    <h6>Ukhra, Near Durgapur, West Bengal</h6>
+                  </Link>
+                </div>
+              </div>
+              <div className="col-6">
+                <div className="button-box">
+                  <Link to="https://sevaapurulia.serviceplace.org.in/" target="_blank">
+                    <img src={sevaapurulialogo} alt='' />
+                    <h6>Sevaa Purulia, West Bengal</h6>
+                  </Link>
+                </div>
+              </div>
+              <div className="col-6">
+                <div className="button-box">
+                  <Link to="https://motinagar.serviceplace.org.in/" target="_blank">
+                    <img src={motinagarlogo} alt='' />
+                    <h6>Moti Nagar, Assam </h6>
+                  </Link>
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="col-6">
-            <div className="button-box">
-              <Link to="https://b2happ.serviceplace.org.in/" target="_blank">
-                <img src={b2hlogo} alt='' />
-                <h6>Near Kharagpur City</h6>
-              </Link>
-            </div>
-          </div>
-          <div className="col-6">
-            <div className="button-box">
-              <Link to="https://rgvnapp.serviceplace.org.in/" target="_blank">
-                <img src={rgvnlogo} alt='' />
-                <h6>Near Morigaon, Assam</h6>
-              </Link>
-            </div>
-          </div>
-          <div className="col-6">
-            <div className="button-box">
-              <Link to="https://ukhraapp.serviceplace.org.in/" target="_blank">
-                <img src={ukhralogo} alt='' />
-                <h6>Ukhra, Near Durgapur, West Bengal</h6>
-              </Link>
-            </div>
-          </div>
-          <div className="col-6">
-            <div className="button-box">
-              <Link to="https://sevaapurulia.serviceplace.org.in/" target="_blank">
-                <img src={sevaapurulialogo} alt='' />
-                <h6>Sevaa Purulia, West Bengal</h6>
-              </Link>
-            </div>
-          </div>
-        </div>
           </div>
         </div>
       </div>
--- src/components/janani/CreateJanani.js
+++ src/components/janani/CreateJanani.js
@@ -62,6 +62,8 @@ function CreateJanani(){
     janani_postal_code: {required: true, value:"", errorClass:"", errorMessage:""},
     janani_service_area: {required: true, value:"", errorClass:"", errorMessage:""},
     special_note: {required: false, value:"", errorClass:"", errorMessage:""},
+    blood_group: {required: false, value:"", errorClass:"", errorMessage:""},
+    emergency_contact_no: {required: false, value:"", errorClass:"", errorMessage:""}
   });
 
   const handleChange = (e) => {
@@ -287,6 +289,10 @@ function CreateJanani(){
       jsonData["janani_postal_code"]            = formData['janani_postal_code'].value;
       jsonData["janani_landmark"]               = formData['janani_landmark'].value;
       jsonData["janani_city"]                   = formData['janani_city'].value;
+
+      jsonData["blood_group"]                   = formData['blood_group'].value;
+      jsonData["emergency_contact_no"]          = formData['emergency_contact_no'].value;
+
       jsonData["is_bpl"]                        = "t";
       jsonData["is_your_personal_number"]       = formData['is_personal_mobile_number'].value;
 
@@ -408,6 +414,24 @@ function CreateJanani(){
             <input type="text" className="form-control" name="hospital_name" id="hospital_name"  onChange={handleChange} placeholder="Involved Hospital Name" value={formData["hospital_name"].value ? formData["hospital_name"].value : ''} />
             <small className="error-mesg">{formData["hospital_name"].errorMessage}</small>
           </div>
+
+          <div className={`form-group ${formData["blood_group"].errorClass}`}>
+            <label htmlFor="blood_group">Blood Group </label>
+            
+            <select className="form-control" onChange={handleChange} value={formData["blood_group"].value || ""} name="blood_group" id="blood_group">
+              <option value="">Select</option>
+              <option value="A+">A+</option>
+              <option value="A-">A-</option>
+              <option value="B+">B+</option>
+              <option value="B-">B-</option>
+              <option value="AB+">AB+</option>
+              <option value="AB-">AB-</option>
+              <option value="O+">O+</option>
+              <option value="O-">O-</option>
+            </select>
+            <small className="error-mesg">{formData["blood_group"].errorMessage}</small>
+          </div>
+
           <div className={`form-group ${formData["is_personal_mobile_number"].errorClass}`}>
             <label className="no-style"><span className="d-block">Is janani's personal mobile number? <span className="text-danger">*</span></span> </label>
             <div className="d-flex">
@@ -427,6 +451,19 @@ function CreateJanani(){
             <input type="tel" className="form-control" name="janani_contact_number" id="janani_contact_number" onChange={handleChange} placeholder="Phone No" value={formData["janani_contact_number"].value ? formData["janani_contact_number"].value : ''} maxLength={10}/>
             <small className="error-mesg">{formData["janani_contact_number"].errorMessage}</small>
           </div>}
+
+          <div className={`form-group ${formData["emergency_contact_no"].errorClass}`}>
+            <label>Other person to contact  </label>
+            <select className="form-control" onChange={handleChange} value={formData["emergency_contact_no"].value || ""} name="emergency_contact_no" id="emergency_contact_no">
+              <option value="">Select</option>
+              <option value="Son">Son</option>
+              <option value="Daughter">Daughter</option>
+              <option value="Spouse">Spouse</option>
+              <option value="Friend">Friend</option>
+            </select>
+            <small className="error-mesg">{formData["emergency_contact_no"].errorMessage}</small>
+          </div>
+
           <div className={`form-group ${formData["whatsapp"].errorClass}`}>
             <label htmlFor="whatsapp">WhatsApp No </label>
             <input type="tel" className="form-control" name="whatsapp" id="whatsapp" onChange={handleChange}  placeholder="WhatsApp No" value={formData["whatsapp"].value ? formData["whatsapp"].value : ''} maxLength={10}/>
